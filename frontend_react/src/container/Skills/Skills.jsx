f2b47a0c00b73bd574f6b6c84fe6262d2fcbe44e
Martijn Wolffenbuttel <martijn.w3@gmail.com>
2025-05-17 21:04:16 +0200

    Added tweaks for experience description elements

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,21 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Skills.scss';
 import { AppWrap } from '../../wrapper';
 import { motion, useAnimationFrame } from 'framer-motion';
 import { images } from '../../constants';
+import { urlFor, client } from '../../client';
 
-const icons = [
-   <img src={images.css} alt="CSS" />,
-   <img src = {images.typescript}></img>,
-   <img src = {images.javascript}></img>,
-   <img src={images.csharp} alt="C#" />,
-   <img src = {images.sql2}></img>,
+// Use an array of objects to link icons to their alt text
+const iconData = [
+  { icon: <img src={images.css} alt="css" />, alt: "css" },
+  { icon: <img src={images.typescript} alt="typescript" />, alt: "typescript" },
+  { icon: <img src={images.javascript} alt="javascript" />, alt: "javascript" },
+  { icon: <img src={images.csharp} alt="csharp" />, alt: "csharp" },
+  { icon: <img src={images.sql2} alt="sql" />, alt: "sql" },
 ];
 
 const Skills = () => {
-  const iconCount = icons.length;
+  const iconCount = iconData.length;
   const [rotation, setRotation] = useState(0);
   const [isHovered, setIsHovered] = useState(false);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [skills, setSkills] = useState([]); 
+  const [selectedIcon, setSelectedIcon] = useState("");
 
   useAnimationFrame((t) => {
     if (!isHovered) {
@@ -23,12 +28,44 @@ const Skills = () => {
     }
   });
 
-return (
+  useEffect(() => {
+    const query = '*[_type == "skills"]';
+    client.fetch(query)
+      .then((data) => setSkills(data));
+  }, [isHovered]);
+
+  const selectedSkill = skills.find(skill => skill.alt === selectedIcon);
+
+  return (
     <motion.div
       className="circle-container"
       style={{ transform: `rotate(${rotation}deg)` }}
     >
-      {icons.map((icon, i) => {
+      {isHovered && selectedSkill && (
+        <div
+          className="circle-center-content"
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: `translate(-50%, -50%) rotate(${-rotation}deg)`,
+            zIndex: 2,
+          }}
+        >
+          <div style={{ marginTop: 20 }}>
+            <div className="skill__text-title head-text">
+              {selectedSkill.skill}
+            </div>
+            <div className="skill__acquired-text bold-text">
+              {selectedSkill.acquiredAt}
+            </div>
+            <div className="skill__acquired-summary p-text">
+              {selectedSkill.summary}
+            </div>
+          </div>
+        </div>
+      )}
+      {iconData.map((item, i) => {
         const angle = (360 / iconCount) * i;
         const iconStyle = {
           transform: `
@@ -38,16 +75,21 @@ return (
             rotate(${-angle - rotation}deg)
           `
         };
+        const isDimmed = hoveredIndex !== null && hoveredIndex !== i;
 
         return (
           <div
             className={`circle-icon icon-${i}`}
             key={i}
-            style={iconStyle}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
+            style={{
+              ...iconStyle,
+              opacity: isDimmed ? 0.2 : 1,
+              transition: 'opacity 0.3s'
+            }}
+            onMouseEnter={() => { setIsHovered(true); setHoveredIndex(i); setSelectedIcon(item.alt); }}
+            onMouseLeave={() => { setIsHovered(false); setHoveredIndex(null); setSelectedIcon(""); }}
           >
-            {icon}
+            {item.icon}
           </div>
         );
       })}
